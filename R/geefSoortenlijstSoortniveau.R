68d0833f5eb6379851ce2abd3136cdba0a336841
LOMMELEN <els.lommelen@inbo.be>
2016-10-26 18:10:37 +0200

    in geefSoortenlijstSoortniveau de mogelijkheid toegevoegd om bovenliggende taxonomische en morfologische groepen ook toe te voegen aan de soortenlijst

--- R/geefSoortenlijstSoortniveau.R
+++ R/geefSoortenlijstSoortniveau.R
@@ -5,50 +5,100 @@
 #' Deze functie geeft voor de gespecifieerde soortengroepen per soortengroep een lijst van alle soorten die hieronder vallen.  Dit betekent dat voor de in de LSVI vermelde genera en soortengroepen als helofieten alle soorten vermeld worden die tot deze groepen behoren.  Voor een lijst van soort(groep)en op het niveau vermeld in de LSVI, wordt beter de functie geefSoortenlijstInvoerniveau() gebruikt.
 #'
 #' @param Soortengroeplijst string waarin de SoortengroepID's na elkaar weergegeven worden, gescheiden door een komma
+#' @param Soortenlijsttype "Soortniveau" betekent dat alle soorten worden weergegeven van de opgegeven soortgroepen, "alle" betekent dat alle soorten en alle taxonomische en morfologische groepen worden weergegeven die volledig in de opgegeven soortgroepen vallen (die aan de parameters voldoen (dus hier wordt de lijst uitgebreid met hogere taxonomische en morfologische groepen).
 #'
 #' @return Deze functie geeft een tabel met velden SoortengroepID, evt. Beschrijving, WetNaam, WetNaamKort en NedNaam (waarbij Beschrijving een omschrijving is voor een groep van soorten binnen eenzelfde indicator).  WetNaam is de volledige Latijnse naam inclusief auteursnaam, WetNaamKort bevat enkel genusnaam en soortnaam (zonder auteursnaam).
 #' 
 #' @examples
-#' geefSoortenlijstSoortniveau("139,142,372,370")
+#' geefSoortenlijstSoortniveau("139,142,370,371")
 #'
 #' @export
 #'
-#' @importFrom dplyr %>% bind_rows mutate_
+#' @importFrom dplyr %>% bind_rows mutate_ filter distinct_
 #' @importFrom RODBC sqlQuery odbcClose
 #'
 #'
 geefSoortenlijstSoortniveau <- 
-  function(Soortengroeplijst){
+  function(Soortengroeplijst, Soortenlijsttype = "alle"){
     #nog controle doen op de invoer!!!
     
-    query <- 
-      sprintf("WITH Soortengroepniveau
-              AS
-              (
-                SELECT Soortengroep.Id as SoortengroepID, 
-                     SoortengroepSoort.SoortensubgroepID, SoortengroepSoort.SoortID, 0 AS Niveau
-                FROM Soortengroep INNER JOIN SoortengroepSoort ON Soortengroep.Id = SoortengroepSoort.SoortengroepID
-                WHERE Soortengroep.Id in (%s)
-                UNION ALL
+    if(Soortenlijsttype == "Soortniveau"){
+      query <- 
+        sprintf("WITH Soortengroepniveau
+                AS
+                (
+                  SELECT Soortengroep.Id as SoortengroepID, 
+                       SoortengroepSoort.SoortensubgroepID, SoortengroepSoort.SoortID, 0 AS Niveau
+                  FROM Soortengroep INNER JOIN SoortengroepSoort ON Soortengroep.Id = SoortengroepSoort.SoortengroepID
+                  WHERE Soortengroep.Id in (%s)
+                  UNION ALL
+                  SELECT Soortengroepniveau.SoortengroepID,
+                       ss2.SoortensubgroepID, ss2.SoortID, Niveau + 1
+                  FROM (Soortengroep AS s2 INNER JOIN SoortengroepSoort AS ss2 ON s2.Id = ss2.SoortengroepID)
+                  INNER JOIN Soortengroepniveau ON s2.Id = Soortengroepniveau.SoortensubgroepID
+                )
+                SELECT Soortengroepniveau.SoortengroepID, 
+                       Soort.WetNaam, Soort.NedNaam
+                FROM Soortengroepniveau 
+                INNER JOIN Soort ON Soortengroepniveau.SoortID = Soort.Id", 
+                Soortengroeplijst)
+      
+      connectie <- connecteerMetLSVIdb()
+      Soortenlijst <- sqlQuery(connectie, query, stringsAsFactors = FALSE)
+      odbcClose(connectie)
+      
+    } else if (Soortenlijsttype == "alle"){
+      query <- 
+        sprintf("WITH Soortengroepniveau
+                AS
+                (
+                  SELECT Soortengroep.Id as SoortengroepID,
+                       SoortengroepSoort.SoortensubgroepID, SoortengroepSoort.SoortID, 0 AS Niveau
+                  FROM Soortengroep INNER JOIN SoortengroepSoort ON Soortengroep.Id = SoortengroepSoort.SoortengroepID
+                  WHERE Soortengroep.Id in (%s)
+                  UNION ALL
+                  SELECT Soortengroepniveau.SoortengroepID,
+                       ss2.SoortensubgroepID, ss2.SoortID, Niveau + 1
+                  FROM (Soortengroep AS s2 INNER JOIN SoortengroepSoort AS ss2 ON s2.Id = ss2.SoortengroepID)
+                  INNER JOIN Soortengroepniveau ON s2.Id = Soortengroepniveau.SoortensubgroepID
+                )
                 SELECT Soortengroepniveau.SoortengroepID,
-                     ss2.SoortensubgroepID, ss2.SoortID, Niveau + 1
-                FROM (Soortengroep AS s2 INNER JOIN SoortengroepSoort AS ss2 ON s2.Id = ss2.SoortengroepID)
-                INNER JOIN Soortengroepniveau ON s2.Id = Soortengroepniveau.SoortensubgroepID
-              )
-              SELECT Soortengroepniveau.SoortengroepID, 
-                     Soort.WetNaam, Soort.NedNaam
-              FROM Soortengroepniveau 
-              INNER JOIN Soort ON Soortengroepniveau.SoortID = Soort.Id", 
-              Soortengroeplijst)
+                       Soortengroep.Naam AS NedNaam_groep, Soortengroep.WetNaam AS WetNaam_groep,
+                       Soortengroeptype.Omschrijving AS Soortengroeptype,
+                       Soort.WetNaam, Soort.NedNaam
+                FROM Soortengroepniveau 
+                LEFT JOIN Soort ON Soortengroepniveau.SoortID = Soort.Id
+                LEFT JOIN (Soortengroep INNER JOIN Soortengroeptype ON Soortengroep.SoortengroeptypeID = Soortengroeptype.Id)
+                    ON Soortengroepniveau.SoortensubgroepID = Soortengroep.Id", 
+                Soortengroeplijst)
+      
+      
+      connectie <- connecteerMetLSVIdb()
+      Soortenlijst <- sqlQuery(connectie, query, stringsAsFactors = FALSE)
+      odbcClose(connectie)
+      
+      Soortenlijst <- Soortenlijst %>%
+        mutate_(
+          NedNaam = ~ifelse(is.na(NedNaam) & Soortengroeptype != "Conceptueel",
+                            NedNaam_groep,
+                            NedNaam),
+          WetNaam = ~ifelse(is.na(WetNaam) & Soortengroeptype != "Conceptueel",
+                            WetNaam_groep,
+                            WetNaam),
+          NedNaam_groep = ~NULL,
+          WetNaam_groep = ~NULL,
+          Soortengroeptype = ~NULL
+        )
+      
+    }
     
-    
-    connectie <- connecteerMetLSVIdb()
-    Soortenlijst <- sqlQuery(connectie, query, stringsAsFactors = FALSE)
-    odbcClose(connectie)
-
      
     #Kolommen met WetNaam (uit tabellen Soortengroep en Soort) samenvoegen, id voor NedNaam, en een kolom WetNaamKort toevoegen
     Soortenlijst <- Soortenlijst %>%
+      distinct_() %>%
+      filter(
+        !is.na(WetNaam) | !is.na(NedNaam)
+      ) %>%
       mutate_(
         WetNaamKort = ~
           gsub(
