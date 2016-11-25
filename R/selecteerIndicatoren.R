75e8be391c07980bb0a7f9a7c2f30c3837ffdcaa
LOMMELEN <els.lommelen@inbo.be>
2016-11-25 09:53:42 +0100

    functie selecteerIndicatoren gemaakt en ingewerkt in andere functies

---
+++ R/selecteerIndicatoren.R
@@ -0,0 +1,110 @@
+#' @title Selecteert indicatoren LSVI op basis van de opgegeven parameters
+#'
+#' @description Deze hulpfunctie selecteert de indicatoren die gebruikt worden voor de bepaling van de Lokale Staat van Instandhouding voor de opgegeven parameters.  Ze is bedoeld om te gebruiken als bouwsteen in andere functies waar de gegevens voor bijvoorbeeld een welbepaalde versie of welbepaalde habitattypes geselecteerd moeten kunnen worden.  
+#'
+#'De parameters kunnen enkel de hieronder gespecifeerde waarden bevatten en moeten als string opgegeven worden.  Default is telkens 'alle', waarbij de soortenlijsten voor alle mogelijke waarden van die parameter weergegeven worden (m.a.w. er is geen selectie voor deze parameter).
+#'
+#' @param Versie De versie van het LSVI-rapport, bv. "Versie 2" of "Versie 3".  Bij de default "alle" worden de gegevens voor de verschillende versies gegeven.
+#' @param Habitatgroep Parameter waarmee alle habitats van een bepaalde habitatgroep kunnen geselecteerd worden, bv. "Bossen", "Heiden", "(Half-)natuurlijke graslanden", "Zoete wateren",...   en "alle" (=default).  Deze waarde moet niet gespecifieerd worden als een bepaald habitat(sub)type geselecteerd wordt.
+#' @param Habitattype Parameter waarmee een habitattype kan geselecteerd worden.  Als dit habitattype meerdere subtypes heeft, zullen de gegevens van alle subtypes van dit habitattype weergegeven worden.
+#' @param Habitatsubtype Parameter waarmee een habitatsubtype geselecteerd kan worden.  Als deze waarde ingevuld is, is het niet nodig om de parameters Habitatgroep en Habitattype te specifieren.
+#' @param Criterium Het LSVI-criterium waarvoor de gegevens geselecteerd worden: "Vegetatie", "Structuur", "Verstoring" of "alle".
+#' @param Indicator De indicator waarvoor de gegevens uit de databank gehaald worden.
+#'
+#' @return Deze functie geeft een tabel met velden Versie, Habitattype, Habitatsubtype, Criterium, Indicator, Indicator_habitatID, SoortengroepID en NiveauSoortenlijstFiche.
+#' 
+#' @examples
+#' selecteerIndicatoren(Versie = "Versie 3", Habitattype = "4010")
+#'
+#' @export
+#'
+#' @importFrom RODBC sqlQuery odbcClose
+#'
+#'
+selecteerIndicatoren <- 
+  function(Versie = geefUniekeWaarden("Versie","VersieLSVI"), 
+           Habitatgroep = geefUniekeWaarden("Habitatgroep","Habitatgroepnaam"),  
+           Habitattype = geefUniekeWaarden("Habitattype","Habitatcode"), 
+           Habitatsubtype = geefUniekeWaarden("Habitatsubtype","Habitatcode_subtype"), 
+           Criterium = geefUniekeWaarden("Criterium","Naam"), 
+           Indicator = geefUniekeWaarden("Indicator","Naam")){
+    match.arg(Versie)
+    match.arg(Habitatgroep)
+    match.arg(Habitattype)
+    match.arg(Habitatsubtype)
+    match.arg(Criterium)
+    match.arg(Indicator)
+    
+    
+    #eerst de selectiegegevens ophalen en de nodige gegevens uit tabel Indicator_habitat, query samenstellen op basis van parameters
+    Parametervoorwaarde <- FALSE
+    query <- "SELECT Versie.VersieLSVI AS Versie, Habitattype.Habitatcode AS Habitattype, 
+    Habitatsubtype.Habitatcode_subtype AS Habitatsubtype,
+    Criterium.Naam AS Criterium, Indicator.Naam AS Indicator, 
+    Indicator_habitat.Id AS Indicator_habitatID,
+    Indicator_habitat.SoortengroepID, Indicator_habitat.NiveauSoortenlijstFiche
+    FROM ((Indicator_habitat 
+    INNER JOIN ((Habitatsubtype INNER JOIN Habitattype ON Habitatsubtype.HabitattypeID = Habitattype.Id)
+    INNER JOIN Habitatgroep ON Habitattype.HabitatgroepID = Habitatgroep.Id)
+    ON Indicator_habitat.HabitatsubtypeID = Habitatsubtype.Id)
+    INNER JOIN (Indicator INNER JOIN Criterium ON Indicator.CriteriumID = Criterium.Id)
+    ON Indicator_habitat.IndicatorID = Indicator.Id)
+    INNER JOIN Versie ON Indicator_habitat.VersieID = Versie.Id"
+    if(Versie[1] != "alle"){
+      query <- sprintf("%s WHERE Versie.VersieLSVI = '%s'", query, Versie)
+      Parametervoorwaarde <- TRUE
+    }
+    if(Habitatgroep[1] != "alle"){
+      if(Parametervoorwaarde){
+        Voegwoord <- "AND"
+      } else {
+        Voegwoord <- "WHERE"
+        Parametervoorwaarde <- TRUE
+      }
+      query <- sprintf("%s %s Habitatgroep.Habitatgroepnaam = '%s'", query, Voegwoord, Habitatgroep)
+    }
+    if(Habitattype[1] != "alle"){
+      if(Parametervoorwaarde){
+        Voegwoord <- "AND"
+      } else {
+        Voegwoord <- "WHERE"
+        Parametervoorwaarde <- TRUE
+      }
+      query <- sprintf("%s %s Habitattype.Habitatcode = '%s'", query, Voegwoord, Habitattype)
+    }
+    if(Habitatsubtype[1] != "alle"){
+      if(Parametervoorwaarde){
+        Voegwoord <- "AND"
+      } else {
+        Voegwoord <- "WHERE"
+        Parametervoorwaarde <- TRUE
+      }
+      query <- sprintf("%s %s Habitatsubtype.Habitatcode_subtype = '%s'", query, Voegwoord, Habitatsubtype)
+    }
+    if(Criterium[1] != "alle"){
+      if(Parametervoorwaarde){
+        Voegwoord <- "AND"
+      } else {
+        Voegwoord <- "WHERE"
+        Parametervoorwaarde <- TRUE
+      }
+      query <- sprintf("%s %s Criterium.Naam = '%s'", query, Voegwoord, Criterium)
+    }
+    if(Indicator[1] != "alle"){
+      if(Parametervoorwaarde){
+        Voegwoord <- "AND"
+      } else {
+        Voegwoord <- "WHERE"
+        Parametervoorwaarde <- TRUE
+      }
+      query <- sprintf("%s %s Indicator.Naam = '%s'", query, Voegwoord, Indicator)
+    }
+    
+    connectie <- connecteerMetLSVIdb()
+    Selectiegegevens <- sqlQuery(connectie, query, stringsAsFactors = FALSE)
+    odbcClose(connectie)
+    
+    return(Selectiegegevens)  
+    
+  }
+
