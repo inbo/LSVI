efc1aa7ecdd6a650b7ba8eaf0af611d81c51c260
LOMMELEN <els.lommelen@inbo.be>
2017-02-09 17:14:47 +0100

    connecteerMetLSVIdb aangepast om paswoord niet in code te vermelden

--- R/connecteerMetLSVIdb.R
+++ R/connecteerMetLSVIdb.R
@@ -1,15 +1,15 @@
 #' @title Connecteer met de databank met LSVI-indicatoren
 #'
-#' @description Deze functie maakt een connectie met de databank met LSVI-indicatoren.  Deze connectie moet altijd geopend worden en meegegeven als argument om andere functies te kunnen gebruiken, want alle functies binnen dit package hebben een databankconnectie nodig om gegevens uit de databank te halen.  Zonder deze connectie werken ze niet.  Binnen INBO kunnen de defaultwaarden gebruikt worden, d.w.z. dat er geen argumenten meegegeven moeten worden in connecteerMetLSVIdb().  Gebruikers buiten INBO hebben een kopie van de databank nodig om met dit package te kunnen werken, en eventueel een aangepaste versie van deze functie.  
+#' @description Deze functie maakt een connectie met de databank met LSVI-indicatoren.  Deze connectie moet altijd geopend worden en meegegeven als argument om andere functies te kunnen gebruiken, want alle functies binnen dit package hebben een databankconnectie nodig om gegevens uit de databank te halen.  Zonder deze connectie werken ze niet.  Binnen INBO kunnen de defaultwaarden gebruikt worden, d.w.z. dat er geen argumenten meegegeven moeten worden in connecteerMetLSVIdb().  Gebruikers buiten INBO hebben een kopie van de databank nodig om met dit package te kunnen werken, en eventueel een aangepaste versie van deze functie.
 #'
 #' @param Server de server waarop de databank staat die aangeroepen wordt (standaard "inbosql03\\prd")
 #' @param Databank de naam van de databank die aangeroepen wordt (standaard "D0122_00_LSVIHabitatTypes")
-#' @param Gebruiker gebruiker van de databank, standaard een gebruiker met leesrechten, andere opties zijn "pc-eigenaar" waarbij de login en wachtwoord gebruikt wordt waarmee ingelogd is op de pc of een eigen gebruikersnaam en wachtwoord ingeven
+#' @param Gebruiker gebruiker van de databank, standaard "pc-eigenaar" waarbij de login en wachtwoord gebruikt wordt waarmee ingelogd is op de pc, andere opties zijn "lezer" waarbij een gebruiker met leesrechten aangemaakt wordt (dit kan enkel als dit geïnstalleerd is op je pc) of een eigen gebruikersnaam en wachtwoord ingeven
 #' @param Wachtwoord wachtwoord van de gebruiker van de databank, standaard een gebruiker met leesrechten, moet niet ingevuld worden als Gebruiker "pc-eigenaar" is
 #'
 #' @return Deze functie geeft een open odbc-connectie naar de gespecifieerde databank.
-#' 
-#' @examples 
+#'
+#' @examples
 #' library(RODBC)
 #' ConnectieLSVIhabitats <- connecteerMetLSVIdb()
 #' sqlQuery(ConnectieLSVIhabitats, "SELECT VersieLSVI, Referentie FROM Versie")
@@ -22,28 +22,43 @@
 #' @importFrom utils file_test
 #'
 
-connecteerMetLSVIdb <- 
+connecteerMetLSVIdb <-
   function(Server = "inbosql03\\prd",
            Databank = "D0122_00_LSVIHabitatTypes",
-           Gebruiker = "D0122_AppR",
-           Wachtwoord = "***REMOVED***"){
-    
+           Gebruiker = "pc-eigenaar",
+           Wachtwoord = "Geen"){
+
   assert_that(is.string(Server))
   assert_that(is.string(Databank))
   assert_that(is.string(Gebruiker))
   assert_that(is.string(Wachtwoord))
-  
-  if(Gebruiker == "pc-eigenaar"){
-    Connectiestring <- 
+
+  if (Gebruiker == "pc-eigenaar") {
+    Connectiestring <-
       sprintf("Driver=SQL Server;Server=%s;Database=%s;Trusted_Connection=Yes;",
               Server, Databank)
   } else {
-    Connectiestring <- 
+    if (Gebruiker == "lezer") {
+      Gebruiker <- "D0122_AppR"
+      Wachtwoord <-
+        tryCatch(Wachtwoord <- scan(file = "./credentials",
+                                    what = "character"),
+                 error = function(e) {
+                   print("Error: Geen wachtwoord gevonden")
+                   return(NULL)
+                 },
+                 warning = function(w) {
+                   print("Error: Geen wachtwoord gevonden")
+                   return(NULL)
+                 },
+                 message = FALSE)
+    }
+    Connectiestring <-
       sprintf("Driver=SQL Server;Server=%s;Database=%s;UID=%s;PWD=%s",
               Server, Databank, Gebruiker, Wachtwoord)
-  } 
-      
-    
-  
+  }
+
+
+
   ConnectieLSVIhabitats <- odbcDriverConnect(Connectiestring)
 }
