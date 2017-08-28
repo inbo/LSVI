c050a712d48908f675f8fb82d6db0d55a5511f93
LOMMELEN <els.lommelen@inbo.be>
2017-08-28 17:10:47 +0200

    verhuis databank naar nieuwe server

--- R/connecteerMetLSVIdb.R
+++ R/connecteerMetLSVIdb.R
@@ -2,7 +2,7 @@
 #'
 #' @description Deze functie maakt een connectie met de databank met LSVI-indicatoren.  Deze connectie moet altijd geopend worden en meegegeven als argument om andere functies te kunnen gebruiken, want alle functies binnen dit package hebben een databankconnectie nodig om gegevens uit de databank te halen.  Zonder deze connectie werken ze niet.  Binnen INBO kunnen de defaultwaarden gebruikt worden, d.w.z. dat er geen argumenten meegegeven moeten worden in connecteerMetLSVIdb().  Gebruikers buiten INBO hebben een kopie van de databank nodig om met dit package te kunnen werken, en eventueel een aangepaste versie van deze functie.
 #'
-#' @param Server de server waarop de databank staat die aangeroepen wordt (standaard "inbosql03\\prd")
+#' @param Server de server waarop de databank staat die aangeroepen wordt (standaard "INBO-SQL07-PRD.inbo.be")
 #' @param Databank de naam van de databank die aangeroepen wordt (standaard "D0122_00_LSVIHabitatTypes")
 #' @param Gebruiker gebruiker van de databank, standaard "pc-eigenaar" waarbij de login en wachtwoord gebruikt wordt waarmee ingelogd is op de pc, andere opties zijn "lezer" waarbij een gebruiker met leesrechten aangemaakt wordt (dit kan enkel als dit geïnstalleerd is op je pc) of een eigen gebruikersnaam en wachtwoord ingeven
 #' @param Wachtwoord wachtwoord van de gebruiker van de databank, standaard de tekst "geen", moet niet ingevuld worden als Gebruiker "pc-eigenaar" is
@@ -23,8 +23,8 @@
 #'
 
 connecteerMetLSVIdb <-
-  function(Server = "inbosql03\\prd",
-           Databank = "D0122_03_LSVIHabitatTypes",
+  function(Server = "INBO-SQL07-PRD.inbo.be",
+           Databank = "D0122_00_LSVIHabitatTypes",
            Gebruiker = "pc-eigenaar",
            Wachtwoord = "geen"){
 
