7b5049db8be482f220127171101261492d090d93
hansvancalster <hans.vancalster@inbo.be>
2018-05-25 14:24:45 +0200

    Voeg functie toe voor berekening van verschilscores per indicator

---
+++ R/berekenVerschilscores.R
@@ -0,0 +1,60 @@
+#' @title Berekent de verschilscores voor de records van een een opgegeven tabel
+#'
+#' @description Deze functie, die bedoeld is als hulpfunctie voor de hoofdfunctie berekenLSVIbasis, berekent de verschilscores van de records van een opgegeven 'statustabel' met velden Rijnr, RefMin, RefMax, Operator, WaardeMin, WaardeMax en TheoretischMaximum. De verschilscores hebben een waarde tussen -1 en +1 en geven negatieve of positieve afwijking ten opzichte van de referentiewaarde.
+#'
+#' @param Statustabel Dataframe met velden Rijnr, RefMin, RefMax, Operator, WaardeMin, WaardeMax en TheoretischMaximum.
+#'
+#'
+#' @return Deze functie geeft een tabel terug met velden Rijnr en Verschilscore
+#'
+#'
+#' @export
+#'
+#' @importFrom assertthat assert_that has_name
+#' @importFrom dplyr %>% mutate select group_by case_when
+#' @importFrom rlang .data
+#'
+#'
+berekenVerschilscores <-
+  function(Statustabel){
+
+    assert_that(inherits(Statustabel, "data.frame"))
+    assert_that(has_name(Statustabel, "Rijnr"))
+    assert_that(has_name(Statustabel, "RefMin"))
+    assert_that(has_name(Statustabel, "RefMax"))
+    assert_that(has_name(Statustabel, "Operator"))
+    assert_that(has_name(Statustabel, "WaardeMin"))
+    assert_that(has_name(Statustabel, "WaardeMax"))
+    assert_that(has_name(Statustabel, "TheoretischMaximum"))
+
+
+    Verschiltabel <- Statustabel %>%
+      mutate(# harmonisch gemiddelde
+        Ref = 2 * .data$RefMin * .data$RefMax / (.data$RefMin + .data$RefMax ),
+        Waarde = 2 * .data$WaardeMin * .data$WaardeMax /
+          (.data$WaardeMin + .data$WaardeMax),
+        # negatieve indicatoren * -1
+        Teken = ifelse(.data$Operator %in% c("<=", "<"), -1, 1),
+        BereikGunstig = case_when(
+          .data$Operator == "<=" ~ .data$Ref,
+          .data$Operator == "<" ~ .data$Ref,
+          .data$Operator == ">=" ~ .data$TheoretischMaximum - .data$Ref,
+          .data$Operator == ">" ~ .data$TheoretischMaximum - .data$Ref,
+          TRUE ~ NA # dus overblijvend geval "=" krijgt NA
+        ),
+        BereikOngunstig = .data$TheoretischMaximum - .data$BereikGunstig,
+        Verschil = (.data$Waarde - .data$Ref) * .data$Teken,
+        Verschilscore = case_when(
+          is.na(.data$Verschil) ~ ifelse(data$Waarde == .data$Ref, 1, -1),
+          .data$Verschil > 0 ~ .data$Verschil / .data$BereikGunstig,
+          .data$Verschil <= 0 ~ .data$Verschil / .data$BereikOngunstig
+        )
+      ) %>%
+      select(
+        .data$Rijnr,
+        .data$Verschilscore
+      )
+
+
+    return(Verschiltabel)
+  }
