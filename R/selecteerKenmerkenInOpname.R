18a9d75ac5b678bede97e0e9326740f393f32107
hansvancalster <hans.vancalster@inbo.be>
2018-06-22 11:54:48 +0200

    Aanpassingen in selecteerKenmerkenInOpname om met extra kolom Vegetatielaag in Data_soortenkenmerken om te kunnen

--- R/selecteerKenmerkenInOpname.R
+++ R/selecteerKenmerkenInOpname.R
@@ -2,19 +2,19 @@
 #'
 #' @description Deze hulpfunctie voor de s4-klassen 'aantal' en 'bedekking' zoekt soorten of kenmerken uit de voorwaarde in de opname en maakt een lijstje van de soorten die voldoen en in de opname voorkomen.  Op basis hiervan kunnen de s4-klassen het totale aantal of de bedekking berekenen.
 #'
-#' 
-#' @param Kenmerken dataframe met alle opgegeven kenmerken, met velden Kenmerk, TypeKenmerk, WaardeMin en WaardeMax
+#'
+#' @param Kenmerken dataframe met alle opgegeven kenmerken, met velden Vegetatielaag, Kenmerk, TypeKenmerk, WaardeMin en WaardeMax
 #' @param Soortengroep dataframe met de soortenlijst die uit Kenmerken gehaald moet worden
 #' @param Studiegroep dataframe met de lijst kenmerken die uit Kenmerken gehaald moet worden
 #' @param SubAnalyseVariabele heeft waarde 'bedekking' als er een subvoorwaarde is voor de bedekking van de geselecteerde soorten of kenmerken
 #' @param SubRefMin minimumwaarde van de grenswaarde voor de bedekking
 #' @param SubRefMax maximumwaarde van de grenswaarde voor de bedekking
 #' @param SubOperator operator voor deze subvoorwaarde: moet de bedekking hoger of lager liggen dan de opgegeven referentiewaarde?
-#' 
-#' @return Deze functie geeft een aangepaste tabel Data_soorten terug waarin enkel de soorten uit de soortenlijst(en) opgenomen zijn en die bovendien gekoppeld is aan de gegevens van de soortenlijst.
-#' 
 #'
-#' @export   
+#' @return Deze functie geeft een aangepaste tabel Data_soorten terug waarin enkel de soorten uit de soortenlijst(en) opgenomen zijn en die bovendien gekoppeld is aan de gegevens van de soortenlijst.
+#'
+#'
+#' @export
 #'
 #' @importFrom dplyr %>% filter left_join inner_join mutate distinct
 #' @importFrom rlang .data
@@ -47,13 +47,17 @@ selecteerKenmerkenInOpname <-
     }
 
     if (length(Studiegroep) > 0) {
-
-      Resultaat <- Kenmerken %>%
-        filter(.data$TypeKenmerk == "studiegroep") %>%
-        inner_join(
-          Studiegroep,
-          by = c("Kenmerk" = "Waarde")
-        )
+      if (exists("Resultaat")) {
+        Resultaat <- Resultaat %>%
+          filter(.data$Vegetatielaag %in% Studiegroep)
+      } else {
+        Resultaat <- Kenmerken %>%
+          filter(.data$TypeKenmerk == "studiegroep") %>%
+          inner_join(
+            Studiegroep,
+            by = c("Kenmerk" = "Waarde")
+          )
+      }
     }
 
     if (!exists("Resultaat")) {
@@ -79,6 +83,9 @@ selecteerKenmerkenInOpname <-
           Operator = SubOperator,
           Rijnr = row_number(.data$Kenmerk)
         )
+    }
+
+
 
       SubStatusberekening <-
         berekenStatus(
