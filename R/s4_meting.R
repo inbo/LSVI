b43e0b956784e6c73e735d4ba8c5f232d9602b64
ToonHub <toon.westra@inbo.be>
2018-07-10 13:21:04 +0200

    add s4-class meting
    voor voorwaarden die niet berekend kunnen worden; de klasse zorgt ervoor dat een NA wordt gegenereerd voor Waarde en dat er een warning wordt gegeven

---
+++ R/s4_meting.R
@@ -0,0 +1,36 @@
+#' S4-klasse voor voorwaarden die enkel rechtstreeks op het terrein gemeten kunnen worden of afgeleid worden uit GIS-data
+#'
+#' Deze klasse meting geeft een waarde NA.  Ze is een nakomeling van de klasse AnalyseVariabele.
+#'
+#' @slot Kenmerken dataframe met alle opgegeven kenmerken, met velden Kenmerk, TypeKenmerk, WaardeMin en WaardeMax
+#'
+#' @importFrom methods setClass setMethod
+#'
+#' @include s4_AnalyseVariabele.R
+setClass(
+  Class = "meting",
+  representation =
+    representation(),
+  contains = "AnalyseVariabele"
+)
+
+setMethod(
+  f = "berekenWaarde",
+  signature = "meting",
+  definition = function(object) {
+
+    warning(paste("De waarde voor de voorwaarde met ID",
+                  object@VoorwaardeID,
+                  "kan niet berekend worden. Geef de waarde voor deze voorwaarde rechtstreeks in als input van de functie 'berekenLSVIBasis' via 'Data_voorwaarden'")) #nolint
+
+    return(c(NA, NA))
+  }
+)
+
+setMethod(
+  f = "geefTheoretischMaximum",
+  signature = "bedekking",
+  definition = function(object) {
+    return(NA)
+  }
+)
