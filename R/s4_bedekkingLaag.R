53d304288dda5e1d517efeaf1a1c7a5841c46831
LOMMELEN <els.lommelen@inbo.be>
2018-07-30 16:20:27 +0200

    s4-klasse bedekkingLaag toegevoegd

---
+++ R/s4_bedekkingLaag.R
@@ -0,0 +1,45 @@
+#' S4-klasse die de totale bedekking van een vegetatielaag berekent
+#'
+#' Deze klasse bedekkingLaag staat in voor de berekening van waarden voor TypeVariabele bedekkingLaag op basis van opgegeven kenmerken.  Ze is een nakomeling van de klasse bedekking.  Ze maakt de berekening op basis van de studiegroep als deze aanwezig is in de opname, en anders op basis van de soortengroep.
+#'
+#' @slot Kenmerken dataframe met alle opgegeven kenmerken, met velden Vegetatielaag, Kenmerk, TypeKenmerk, WaardeMin en WaardeMax
+#'
+#' @importFrom methods setClass setMethod
+#'
+#' @include s4_bedekking.R
+setClass(
+  Class = "bedekkingLaag",
+  representation =
+    representation(),
+  contains = "bedekking"
+)
+
+setMethod(
+  f = "berekenWaarde",
+  signature = "bedekkingLaag",
+  definition = function(object) {
+
+    Test <- object@Kenmerken %>%
+      filter(
+        .data$TypeKenmerk == "studiegroep",
+        .data$Kenmerk %in% object@Studiegroep$Waarde,
+        !is.na(.data$WaardeMax)
+      )
+    
+    if (nrow(Test) > 0) {
+      object@Soortengroep <- data.frame()
+      berekenWaarde(as(object, "bedekking"))
+    } else {
+      object@Studiegroep <- data.frame()
+      berekenWaarde(as(object, "bedekking"))
+    }
+  }
+)
+
+setMethod(
+  f = "geefTheoretischMaximum",
+  signature = "bedekkingLaag",
+  definition = function(object) {
+    return(100)
+  }
+)
