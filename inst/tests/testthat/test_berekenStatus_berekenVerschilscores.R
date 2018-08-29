242c2221fa1d77f22efca0651fe7984cba6eabf6
LOMMELEN <els.lommelen@inbo.be>
2018-08-29 10:32:01 +0200

    Test voor invoerwaarde ja/nee en referentiewaarde ja/nee

--- inst/tests/testthat/test_berekenStatus_berekenVerschilscores.R
+++ inst/tests/testthat/test_berekenStatus_berekenVerschilscores.R
@@ -1,5 +1,7 @@
 context("test berekenStatus en berekenVerschilscores")
 
+library(dplyr)
+
 describe("berekeningen gebeuren correct", {
   it("operator = wordt correct berekend", {
     Testdata <-
@@ -15,6 +17,44 @@ describe("berekeningen gebeuren correct", {
       all.equal(
         berekenVerschilscores(Testdata),
         data.frame(Rijnr = 1, Verschilscore = 0)
+      )
+    )
+  })
+  
+  it("invoerwaarde 'ja/nee' wordt correct behandeld", {
+    Testdata <-
+      data.frame(
+        Rijnr = 1, RefMin = 1, RefMax = NA, Operator = "<=",
+        WaardeMin = 1,
+        WaardeMax = 1, TheoretischMaximum = 1, TypeVariabele = "Ja/nee",
+        stringsAsFactors = FALSE)
+    expect_equal(
+      berekenStatus(Testdata),
+      tibble(Rijnr = 1, Status = TRUE)
+    )
+    stopifnot(
+      all.equal(
+        berekenVerschilscores(Testdata),
+        data.frame(Rijnr = 1, Verschilscore = -1)
+      )
+    )
+  })
+  
+  it("referentiewaarde 'ja/nee' wordt correct behandeld", {
+    Testdata <-
+      data.frame(
+        Rijnr = 1, RefMin = 1, RefMax = NA, Operator = "<=",
+        WaardeMin = 1,
+        WaardeMax = NA, TheoretischMaximum = 1, TypeVariabele = "Ja/nee",
+        stringsAsFactors = FALSE)
+    expect_equal(
+      berekenStatus(Testdata),
+      tibble(Rijnr = 1, Status = TRUE)
+    )
+    stopifnot(
+      all.equal(
+        berekenVerschilscores(Testdata),
+        data.frame(Rijnr = 1, Verschilscore = 1)
       )
     )
   })
