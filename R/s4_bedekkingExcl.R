af8a6e4151b23ce594874faa5d4aae20e8344096
LOMMELEN <els.lommelen@inbo.be>
2018-12-04 17:14:46 +0100

    s4-klasse bedekkingExcl toegevoegd (issue #107)

---
+++ R/s4_bedekkingExcl.R
@@ -0,0 +1,68 @@
+#' S4-klasse die de totale bedekking van de soorten berekent die niet in de lijst staan
+#'
+#' Deze klasse BedekkingExcl staat in voor de berekening van waarden voor AnalyseVariabele BedekkingExcl op basis van opgegeven kenmerken.  Ze is een nakomeling van de klasse AnalyseVariabele.  Ze berekent de totale bedekking van soorten die niet in de opgegeven soortenlijst staan (dus alle soorten behalve soorten uit soorten uit lijst).
+#'
+#' @slot Kenmerken dataframe met alle opgegeven kenmerken, met velden Vegetatielaag, Kenmerk, TypeKenmerk, WaardeMin en WaardeMax
+#'
+#' @importFrom methods setClass setMethod
+#'
+#' @include s4_AnalyseVariabele.R
+setClass(
+  Class = "bedekkingExcl",
+  representation =
+    representation(),
+  contains = "AnalyseVariabele"
+)
+
+setMethod(
+  f = "berekenWaarde",
+  signature = "bedekkingExcl",
+  definition = function(object) {
+
+
+    object@Kenmerken <- object@Kenmerken %>%
+      filter(
+        is.na(.data$Eenheid) |
+          (!.data$Eenheid %in% c("Grondvlak_ha", "Volume_ha"))
+      )
+
+    Resultaat <-
+      deselecteerKenmerkenInOpname(
+        object@Kenmerken,
+        object@Soortengroep,
+        object@Studiegroep,
+        object@SubAnalyseVariabele,
+        object@SubRefMin,
+        object@SubRefMax,
+        object@SubOperator
+      )
+
+    if (length(Resultaat) == 1 & all(is.na(Resultaat))) {
+      return(NA)
+    }
+
+    #Als er meer NA's zijn in WaardeMax dan in WaardeMin, wil dit zeggen dat
+    #er aan-/afwezigheden opgegeven zijn in plaats van bedekkingen
+    #In dat geval berekenen we geen bedekking en geven we een warning
+    if (sum(is.na(Resultaat$WaardeMin)) < sum(is.na(Resultaat$WaardeMax))) {
+      BedekkingMin <- NA
+      BedekkingMax <- NA
+      warning("De bedekking is niet berekend voor indicatoren waarbij voor sommige soorten of kenmerken enkel aan- of afwezigheid opgegeven is") #nolint
+    } else {
+      BedekkingMin <-
+        (1.0 - prod( (1.0 - Resultaat$WaardeMin), na.rm = TRUE))
+      BedekkingMax <-
+        (1.0 - prod( (1.0 - Resultaat$WaardeMax), na.rm = TRUE))
+    }
+
+    return(c(BedekkingMin, BedekkingMax))
+  }
+)
+
+setMethod(
+  f = "geefTheoretischMaximum",
+  signature = "bedekkingExcl",
+  definition = function(object) {
+    return(1)
+  }
+)
---
+++ inst/tests/testthat/test_s4_bedekkingExcl.R
@@ -0,0 +1,33 @@
+context("test s4_BedekkingExcl")
+
+library(methods)
+
+describe("s4_BedekkingExcl", {
+
+  it("Berekening BerekenWaarde gebeurt correct", {
+    expect_equal(
+      berekenWaarde(
+        new(
+          Class = "bedekkingExcl",
+          Kenmerken =
+            data.frame(
+              Kenmerk = c("A1", "B2", "C1", "D3", "E1"),
+              TypeKenmerk = "soort_nbn",
+              WaardeMin = c(1, 0, 1, 0, 1),
+              WaardeMax = 1,
+              Eenheid = "%",
+              stringsAsFactors = FALSE
+            ),
+          Soortengroep =
+            data.frame(
+              NbnTaxonVersionKey = c("A1", "B1", "C1", "E1"),
+              TaxonId = 1,
+              SubTaxonId = 2,
+              stringsAsFactors = FALSE
+            )
+        )
+      ),
+      c(0, 1)
+    )
+  })
+})
