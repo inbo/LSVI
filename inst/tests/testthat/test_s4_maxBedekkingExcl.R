55d96f6914f58aac216eb557fa3370c4ed94bd91
ElsLommelen <els.lommelen@inbo.be>
2020-02-25 19:22:32 +0100

    kleine aanpassing om nieuwe check te triggeren

--- inst/tests/testthat/test_s4_maxBedekkingExcl.R
+++ inst/tests/testthat/test_s4_maxBedekkingExcl.R
@@ -38,7 +38,7 @@ describe("s4_maxBedekkingExcl", {
               Kenmerk = c("A1", "B2", "C2", "D3", "E2"),
               TypeKenmerk = "soort_nbn",
               WaardeMin = c(0.5, 0, 0.6, 0.2, 0.3),
-              WaardeMax = c(0.7, 0, 0.8, 0.7, 0.3),
+              WaardeMax = c(0.7, 0, 0.8, 0.7, 0.4),
               Eenheid = "%",
               stringsAsFactors = FALSE
             ),
