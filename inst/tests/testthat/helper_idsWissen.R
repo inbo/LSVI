188a4859cb6640187f2a262172ef0cef4e0922d8
ElsLommelen <els.lommelen@inbo.be>
2021-09-02 17:48:31 +0200

    helper function idsWissen(): remove attribute 'problems' (probably generated by DBI)

--- inst/tests/testthat/helper_idsWissen.R
+++ inst/tests/testthat/helper_idsWissen.R
@@ -1,6 +1,14 @@
 #'@importFrom dplyr %>% mutate
 
 idsWissen <- function(Resultaat) {
+  Resultaat_detail <-
+    Resultaat[["Resultaat_detail"]] %>%
+    mutate(
+      BeoordelingID = NULL,
+      Combinatie = NULL,
+      VoorwaardeID = NULL
+    )
+  attr(Resultaat_detail, "problems") <- NULL
   Resultaat <-
     list(
       Resultaat_criterium = Resultaat[["Resultaat_criterium"]],
@@ -9,13 +17,7 @@ idsWissen <- function(Resultaat) {
         mutate(
           BeoordelingID = NULL
         ),
-      Resultaat_detail =
-        Resultaat[["Resultaat_detail"]] %>%
-        mutate(
-          BeoordelingID = NULL,
-          Combinatie = NULL,
-          VoorwaardeID = NULL
-        ),
+      Resultaat_detail = Resultaat_detail,
       Resultaat_globaal = Resultaat[["Resultaat_globaal"]]
     )
   return(Resultaat)
